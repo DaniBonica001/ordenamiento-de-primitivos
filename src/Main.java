46895f742273c0cd4bff43c7bc942b5c146516ce
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-02-23 19:00:34 -0500

    Second commit: I found how to use the BufferedReader and the BufferedWriter. I made a method to receive the String case to converter in an array. I made another method to converter the String array to a double array.

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,52 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	
-	public static int agesArray[];
+	public static double []caseArray;
+	public final static String SPLIT=" ";
+	public final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public final static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+	
 
 	public static void main(String[] args) throws IOException{		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+
 		
-		
-		//bw.write("Indique la cantidad de casos que serán entregados");
-		System.out.println("Indique la cantidad de casos que serán entregados");
+		bw.write("Indique la cantidad de casos que serán entregados: ");
+		bw.flush();
 		int cases = br.read();
 		
-		
-		
-		
+		/*
+		int counter=0;
+		while(counter<cases) {
+			//bw.write("+++++++++++"+counter);
+			receiveCases();
+			caseArray= returnArrayDouble();			
+			counter++;
+		}
+		*/
+				
+		bw.write("Bye :(");
 		br.close();
 		bw.close();
-		
-
+	}
+	
+	
+	public static String [] receiveCases() throws IOException {
+		bw.write("Ingrese el caso de prueba, cada numero separado por espacios: ");
+		String ageCase= br.readLine();
+		bw.flush();
+		String[] parts= ageCase.split(SPLIT);		
+		return parts;		
+	}
+	
+	public static double[] returnArrayDouble() throws IOException {
+		String [] ageString = receiveCases();
+		double []ages= new double [ageString.length];
+		for (int i=0;i<ages.length;i++) {
+			if (ageString[i]!=" ") {
+				ages[i]=Double.parseDouble(ageString[i]);
+			}
+		}
+		return ages;		
 	}
 
 }
