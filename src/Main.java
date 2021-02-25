89bac87f3b7f7e3acb651ed77bd2b1e9daab6a9e
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-02-25 07:15:32 -0500

    Basically I change everything because I saw the video about how it supposed the program have to work so I did all the code on another project and I copied it on this projtect. I change the method to receive the information because I had problems with the Buffered. Despite of that, everythig is kind of the same.

--- src/Main.java
+++ src/Main.java
@@ -3,87 +3,93 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
-
 public class Main {
 	
-	public static double []caseArray;
+	public static ArrayList<String>ageNumbers;
 	public final static String SPLIT=" ";
 	public final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public final static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
 	
-
 	public static void main(String[] args) throws IOException{		
-
+		ageNumbers = new ArrayList<String>();
 		
-		bw.write("Indique la cantidad de casos que serán entregados: ");
+		bw.write("Indique la cantidad de casos a ingresar: ");
 		bw.flush();
-		int cases = br.read();
-		
-		receiveCases();
-		caseArray= returnArrayDouble();	
-		bw.flush();
-		bw.write(organizeArray(caseArray));
-		/*
-		int counter=0;
-		while(counter<cases) {
-			//bw.write("+++++++++++"+counter);
-			receiveCases();
-			caseArray= returnArrayDouble();			
-			counter++;
+		int cases = Integer.parseInt(br.readLine());
+			
+		int i=0;
+		while(i<cases) {			
+			receiveCases();			
+			i++;
 		}
-		*/
+		
+		System.out.println("salida: "+"\n"+showOut());
 				
 		br.close();
 		bw.close();
 	}
 	
 	
-	public static String [] receiveCases() throws IOException {
-		bw.write("Ingrese el caso de prueba, cada numero separado por espacios:");
-		String ageCase= br.readLine();
-		bw.flush();
-		String[] parts= ageCase.split(SPLIT);		
-		return parts;		
+	public static void receiveCases() throws IOException {
+		String line;
+		try {	
+			line= br.readLine();
+			if (line!=null) {				
+				String []parts = line.split(SPLIT);				
+				stringToDouble(parts);				
+			}
+		
+		} catch (IOException e) {
+			System.out.println("Something went wrong.");			
+			e.printStackTrace();
+		}		  	
 	}
 	
-	public static double[] returnArrayDouble() throws IOException {
-		String [] ageString = receiveCases();
-		double []ages= new double [ageString.length];
-		for (int i=0;i<ages.length;i++) {
-			if (ageString[i]!=" ") {
-				ages[i]=Double.parseDouble(ageString[i]);
-			}
-		}
-		return ages;		
+	public static void stringToDouble(String [] ageArray) throws IOException {
+		if (ageArray!=null) {
+			double [] ages =new double[ageArray.length];
+			for (int i=0;i<ages.length;i++) {
+				ages[i]=Double.parseDouble(ageArray[i]);		
+			}			
+			bubbleSort(ages);				
+		}	
 	}
+	
 
-	public static String organizeArray(double[]array) {
-		int amountChanges=0;
+	public static void bubbleSort(double[]numbers) {
+		//System.out.println("Si uso el método bubble");
 		int pasadas=0;
-		for (int i=1;i<array.length;i++) {
-			int changes=0;			
-			for (int j=0;j<array.length-i;j++) {
-				if (array[j]>array[j+1]) {
-					double temp = array[j]; 
-					array[j]=array[j+1];
-					array[j+1]=temp;
+		int amountChanges=0;
+
+		for (int i=1;i<numbers.length;i++) {
+			int changes=0;
+
+			for (int j=0;j<numbers.length-i;j++) {
+				if (numbers[j]>numbers[j+1]) {
+					double temp= numbers[j];
+					numbers[j]= numbers[j+1];
+					numbers[j+1]=temp;
 					changes++;					
-				}				
-								
-				System.out.println("\n"+Arrays.toString(array));
-				System.out.println("changes: "+changes);
-				
+				}
 			}
 			amountChanges+=changes;
 			pasadas++;
-			System.out.println("amountChanges:"+amountChanges);
-			System.out.println("pasadas:"+pasadas);
-			
 		}
-		double average = (double) amountChanges/pasadas;	
-		
-		return average+"-"+Arrays.toString(array);	
-	}
 
+		double average =(double) amountChanges/pasadas;
+		double roundAverage = Math.round(average*100.0)/100.0;
+		String message= roundAverage+"-"+Arrays.toString(numbers);	
+		ageNumbers.add(message);	
+	}
+	
+
+	public static String showOut() {
+		String message="";
+		for (int i=0;i<ageNumbers.size();i++) {
+			message+=ageNumbers.get(i)+"\n";			
+		}
+		return message;
+	}
 }
